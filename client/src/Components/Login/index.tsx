ac3d26c7d16ea31d285ac76497b4647dc94354c8
Tega Oke <oketegah@gmail.com>
2019-07-03 16:29:04 +0100

    Copleted login modal

--- client/src/Components/Login/index.tsx
+++ client/src/Components/Login/index.tsx
@@ -2,7 +2,29 @@ import React from 'react';
 import './Login.css';
 
 function Login() {
-  return <div className="loginForm">Login</div>;
+  return (
+    <div className="loginForm">
+      <p>POLLARK</p>
+      <p>LOGIN</p>
+      <form>
+        <input type="text" placeholder="Username/Email" />
+        <input type="password" placeholder="Password" />
+        <br />
+        <button type="submit">LOGIN</button>
+      </form>
+      <ul>
+        <li>
+          <i className="mdi mdi-google" />
+        </li>
+        <li>
+          <i className="mdi mdi-linkedin" />
+        </li>
+      </ul>
+      <p>
+        Forgot your password? <a href="/">click here</a>
+      </p>
+    </div>
+  );
 }
 
 export default Login;
