bb341c5109d387fa930e56eeabd3d065012fa5da
Tega Oke <oketegah@gmail.com>
2019-07-05 08:01:03 +0100

    Completed masonry grid system for dashboard

--- client/src/Components/Dashboard/Voter/Events/index.tsx
+++ client/src/Components/Dashboard/Voter/Events/index.tsx
@@ -1,9 +1,63 @@
 import React from 'react';
 
+import Masonry from 'react-masonry-component';
+
 import './Events.css';
 
 function Events(props: any): any {
-  return <p>yo</p>;
+  return (
+    <div className="grid">
+      <div className="grid-sizer" />
+      <Masonry>
+        <div className="grid-item">
+          <div className="dim" />
+          <img
+            alt="img"
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/orange-tree.jpg"
+          />
+        </div>
+        <div className="grid-item">
+          <div className="dim" />
+          <img
+            alt="img"
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/submerged.jpg"
+          />
+        </div>
+        <div className="grid-item">
+          <div className="dim" />
+
+          <img
+            alt="img"
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/look-out.jpg"
+          />
+        </div>
+        <div className="grid-item">
+          <div className="dim" />
+
+          <img
+            alt="img"
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/one-world-trade.jpg"
+          />
+        </div>
+        <div className="grid-item">
+          <div className="dim" />
+
+          <img
+            alt="img"
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/drizzle.jpg"
+          />
+        </div>
+        <div className="grid-item">
+          <div className="dim" />
+
+          <img
+            alt="img"
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/cat-nose.jpg"
+          />
+        </div>
+      </Masonry>
+    </div>
+  );
 }
 
 export default Events;
